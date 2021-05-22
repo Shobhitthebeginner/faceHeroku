c788ad61932d13a12cc3b9cec0eca1b0f46e390d
Shobhitthebegineer <shobhit.18bcs1107@abes.ac.in>
2021-05-22 20:55:19 +0530

    appchanged

--- app.py
+++ app.py
@@ -19,7 +19,6 @@ import requests
 from PIL import Image
 from io import BytesIO
 import numpy as np
-
 config = {
     "type": "service_account",
     "project_id": "image-23704",
@@ -38,23 +37,32 @@ cred = credentials.Certificate(config)
 firebase_admin.initialize_app(cred)
 db = firestore.client()
 user = db.collection('image').document('images').get().to_dict()
-Name = list(user.keys())
-Urls = list(user.values())
-images = []
-classNames = []
+Name=list(user.keys())
+Urls=list(user.values())
+images=[] # containing matrix
+classNames=[]
+print(Urls)
+for i in range(len(Urls)):
+    req = urllib.request.urlopen(Urls[i])
+    arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
+    img = cv2.imdecode(arr, -1)  # 'Load it as it is'
+    # cv2.imshow('lalala', img)
+    # cv2.waitKey(0)
+    images.append(img)
+    classNames.append(Name[i])
+
 def findEncodings(images):
-    encodeList = []
+    encodeList=[]
     for img in images:
         img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-        encode = face_recognition.face_encodings(img)[0]
+        encode=face_recognition.face_encodings(img)[0]
         encodeList.append(encode)
     return encodeList
-
-
+encodeListKnown = findEncodings(images)
 def markAttendance(name):
-    with open('Attendance.csv', 'r+') as f:
+    with open('Attendance.csv','r+') as f:
         myDataList = f.readlines()
-        nameList = []
+        nameList=[]
         for line in myDataList:
             entry = line.split(',')
             nameList.append(entry[0])
@@ -62,53 +70,76 @@ def markAttendance(name):
             now = datetime.now()
             dtString = now.strftime('%H:%M:%S')
             f.writelines(f'\n{name},{dtString}')
+# imgElon = face_recognition.load_image_file('ImageBasics/Elon musk.jpg')  # loading image
+# imgElon = cv2.cvtColor(imgElon,cv2.COLOR_BGR2RGB)          # Converting BGR to RGB
+
+imgTest = face_recognition.load_image_file('vaibhav.jpg')
+imgTest = cv2.cvtColor(imgTest,cv2.COLOR_BGR2RGB)
+faceCascade = cv2.CascadeClassifier(cv2.data.haarcascades + "haarcascade_frontalface_default.xml")
+gray = cv2.cvtColor(imgTest, cv2.COLOR_BGR2GRAY)
+faces = faceCascade.detectMultiScale(
+        gray,
+        scaleFactor=1.3,
+        minNeighbors=3,
+        minSize=(30, 30)
+)
+
+# Finding faces in our image
+# while True:
+#     faceloc = face_recognition.face_locations(images[0])[0]   # finding face in picture
+#     encodeElon = face_recognition.face_encodings(images[0])[0]  # encode the detected face
+#     cv2.rectangle(images[0],(faceloc[3],faceloc[0]),(faceloc[1],faceloc[2]),(255,0,255),2)    # detected face location
+#
+#
+# # Finding faces in our test image
+#     facelocTest= face_recognition.face_locations(imgTest)[0]   # finding face in picture
+#     encodeTest = face_recognition.face_encodings(imgTest)[0]  # encode the detected face
+#     cv2.rectangle(imgTest,(facelocTest[3],facelocTest[0]),(facelocTest[1],facelocTest[2]),(255,0,255),2)
+#     print(facelocTest)
+#     break
+#
+# # comparing between faces and finding measurements(distance between them)
+# # comparing the two images using linear encoding
+#
+#     results = face_recognition.compare_faces([encodeElon],encodeTest)
+#
+# # for finding the best match we will calculate the distance
+#     faceDis=face_recognition.face_distance([encodeElon],encodeTest)  # lower the faceDis more chances of matching
+#     print(results,faceDis)
+#     cv2.putText(imgTest,f'{results}{round(faceDis[0],2)}',(10,40),cv2.FONT_HERSHEY_COMPLEX,1,(0,0,255),2)
+#
+#     cv2.imshow('Elon Musk',images[0])         # Showing image
+#     cv2.imshow('Elon Test',imgTest)
+#     cv2.waitKey(0)
+
+i=0
+while True:
+    for (x, y, w, h) in faces:
+        cv2.rectangle(imgTest, (x, y), (x + w, y + h), (0, 255, 0), 2)
+        roi_color = imgTest[y:y + h, x:x + w]
+        img=roi_color
+        imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
+        imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
+        facesCurrFrame = face_recognition.face_locations(imgS)
+        encodeCurrFrame = face_recognition.face_encodings(imgS, facesCurrFrame)
+        for encodeFace, faceloc in zip(encodeCurrFrame, facesCurrFrame):
+            matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
+            faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
+            print(faceDis)
+            matchIndex = np.argmin(faceDis)
+            if matches[matchIndex]:
+                name = classNames[matchIndex].upper()
+                # print(name)
+                y1, x2, y2, x1 = faceloc
+                y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
+                cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
+                cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), 2, cv2.FILLED)
+                cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0), 2)
+                markAttendance(name)
+        if (cv2.waitKey(20) & 0xFF == ord('q')):
+            break
+
+        cv2.imshow('WebCam', img)
 
 
-
-
-
-app = flask.Flask(__name__)
-
-
-@app.route('/', methods=['GET'])
-def home():
-    try:
-        print('hfisdnfsdnsdnfsdfnsldfl')
-        for i in range(len(Urls)):
-            req = urllib.request.urlopen(Urls[i])
-            arr = np.asarray(bytearray(req.read()), dtype=np.uint8)
-            img = cv2.imdecode(arr, -1)
-            images.append(img)
-            classNames.append(Name[i])
-        encodeListKnown = findEncodings(images)
-        cpp = cv2.VideoCapture(0)
-        while True:
-            success, img = cpp.read()
-            imgS = cv2.resize(img, (0, 0), None, 0.25, 0.25)
-            imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
-            facesCurrFrame = face_recognition.face_locations(imgS)  # faces location in webcam
-            encodeCurrFrame = face_recognition.face_encodings(imgS, facesCurrFrame)
-
-            for encodeFace, faceloc in zip(encodeCurrFrame, facesCurrFrame):
-                matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-                faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-                print(faceDis)
-                matchIndex = np.argmin(faceDis)
-                if matches[matchIndex]:
-                    name = classNames[matchIndex].upper()
-                    y1, x2, y2, x1 = faceloc
-                    y1, x2, y2, x1 = y1 * 4, x2 * 4, y2 * 4, x1 * 4
-                    cv2.rectangle(img, (x1, y1), (x2, y2), (0, 255, 0), 2)
-                    cv2.rectangle(img, (x1, y2 - 35), (x2, y2), (0, 255, 0), 2, cv2.FILLED)
-                    cv2.putText(img, name, (x1 + 6, y2 - 6), cv2.FONT_HERSHEY_COMPLEX, 1, (255, 255, 0), 2)
-                    markAttendance(name)
-            if (cv2.waitKey(20) & 0xFF == ord('q')):
-                break
-
-            cv2.imshow('WebCam', img)
-        cpp.release()
-        return jsonify({"response" : 'hi this is python'})
-    except KeyError:
-        print('hfisdnfsdnsdnfsdfnsldfl')
-        return jsonify({"response" : 'error'})
 
